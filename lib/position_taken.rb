d561097cd8b047dd21def5581c4b55fe94f12847
Jung Lee <lee.jung.s95@gmail.com>
2020-04-08 03:05:03 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,5 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  if board[index] == " " || "  "
-    false
-  elsif board[index] == "nil"
-    false
-  else 
+  board[index] = "nil"
 end
